a65763801c7344bc292e849114ff98eede3d2bba
buffard <samuelwebber19@gmail.com>
2018-10-23 11:46:21 -0500

    completed the daily journal pt3 added a json server and re arranged the js

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,46 +1,18 @@
-const journalEntries = [
-  {
-    concept: "Array methods",
-    date: "07/24/2018",
-    entry: "We learned about array methods, but only forEach made sense",
-    mood: "Ok"
-  },
-  {
-    concept: "Do I Work",
-    date: "10/22/2018",
-    entry: "I just want this to work, then I'll understand",
-    mood: "Happy"
-  }
-]
-
-/*
-  Purpose: To create, and return, a string template that
-  represents a single journal entry object as HTML
-
-  Arguments: journalEntry (object)
-*/
-const makeJournalEntryComponent = (journalEntry) => {
-  // Create your own HTML structure for a journal entry
-  return `
-    <div id="journalEntry">
-      <h1>${journalEntry.concept}</h1>
-      <h2>${journalEntry.date}</h2>
-      <p>${journalEntry.entry}</p>
-      <p>${journalEntry.mood}</p>
-    </div>
-  `
-}
-
-// /*
-//     Purpose: To render all journal entries to the DOM
-//     Arguments: entries (array of objects)
-// */
-
-const renderJournalEntries = (entries) => {
-  journalEntries.forEach((a) => {
-    document.querySelector(".entryLog").innerHTML += makeJournalEntryComponent(a)
+// Fetch from the API
+fetch("http://localhost:3000/entries")
+  .then(entries => entries.json())  // Parse as JSON
+  .then(parsedEntries => {
+    parsedEntries.forEach((entry) => {
+      const makeJournalEntryComponent = (journalEntry) => {
+        return `
+          <div id="journalEntry">
+            <h1>${entry.concept}</h1>
+            <h2>${entry.date}</h2>
+            <p>${entry.entry}</p>
+            <p>${entry.mood}</p>
+          </div>
+        `
+      }
+      document.querySelector(".entryLog").innerHTML += makeJournalEntryComponent(parsedEntries)
+    })
   })
-}
-
-// Invoke the render function
-renderJournalEntries(journalEntries)
